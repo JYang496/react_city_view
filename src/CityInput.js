94746c17ca2db379be87b2e8ab6e20444fe72de7
Jinhua Yang <jyang496@uwo.ca>
2023-07-01 14:49:20 -0400

    API and consts

--- src/CityInput.js
+++ src/CityInput.js
@@ -1,12 +1,44 @@
+import {useState} from "react";
+import {AccessKey, BaseUrl, DefaultCity} from "./consts";
+import axios from "axios";
+
 const CityInput = () =>{
 
+    const [city, setCity] = useState(DefaultCity)
+    const [images, setImages] =useState()
     // Call back event handler for key down in search
     const cbInput = (evt) =>{
-        evt.key === "Enter" &&
+        let newCity = evt.target.value.trim().toLowerCase()
+        evt.key === "Enter" && newCity !== city &&
         (()=>{
-            let city = evt.target.value.trim().toLowerCase()
+            setCity(newCity)
             console.log(city)
+            fetchCity()
         })()
+    }
+
+    const fetchCity = () =>{
+        axios.get(BaseUrl, {
+            params:{
+                query: city,
+                orientation: 'landscape',
+            },
+            headers:{
+                Authorization: `Client-ID ${AccessKey}`
+            }
+        })
+            .then(res =>{
+                let {data: {results}} = res
+                console.log('raw data:', results)
+
+                let imgList = results.map(item =>({
+                    description: item.alt_description,
+                    regular:item.urls.regular,
+                    thumb: item.urls.thumb
+                }))
+                setImages(imgList)
+            })
+            .catch(err => console.log("Error during fetching!"))
     }
 
     return <div>
@@ -14,6 +46,7 @@ const CityInput = () =>{
         placeholder="Search"
         onKeyDown={cbInput}
         />
+        {JSON.stringify(images)}
     </div>
 }
 
---
+++ src/consts.js
@@ -0,0 +1,3 @@
+export const DefaultCity = 'Toronto'.toLowerCase()
+export const BaseUrl = 'https://api.unsplash.com/search/photos'
+export const AccessKey = 'Yljt_h6ZBTA0jib3FSZFl2z9gWt9CbiIPYADyguoZu0'
